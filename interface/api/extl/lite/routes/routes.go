b641c305a7271e15b51f8b3eb119d0a4058c7e09
rafsanjani94s <ali.rafsanjani@majoo.id>
2022-11-22 10:19:30 +0700

    health check routes

--- interface/api/extl/lite/routes/routes.go
+++ interface/api/extl/lite/routes/routes.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func API(e *echo.Echo) {
-
+	lite := e.Group("/lite")
+	lite.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "health check lite routes!")
+	})
 }
--- interface/api/extl/weborder/v1/routes/routes.go
+++ interface/api/extl/weborder/v1/routes/routes.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func API(e *echo.Echo) {
-
+	v1 := e.Group("/v1")
+	v1.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "health check v1 routes!")
+	})
 }
