30d63da64fea667c8d9382f8c30a78fdae076498
rafsanjani94s <ali.rafsanjani@majoo.id>
2022-11-24 13:26:14 +0700

    utils response

---
+++ utils/response/error_response.go
@@ -0,0 +1,58 @@
+package common
+
+import (
+	"e-menu-tentakel/utils/config"
+	"errors"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type ErrorResponse struct {
+	Message          string            `json:"message"`
+	ErrorValidations map[string]string `json:"error_validation,omitempty"`
+}
+
+func GenerateResponseError(err error, data ...map[string]string) interface{} {
+	var ve validator.ValidationErrors
+	var message string
+
+	if errors.As(err, &ve) {
+		var out = make(map[string]string)
+
+		for _, fe := range ve {
+			field := fe.Namespace()
+			if field == "" {
+				field = "field"
+			}
+
+			out[field] = fe.Translate(config.Translate)
+
+			if message == "" {
+				message = fe.Translate(config.Translate)
+			}
+		}
+		return &ErrorResponse{
+			Message:          message,
+			ErrorValidations: removeFirstStruct(out),
+		}
+	}
+
+	var errorVal map[string]string
+	if len(data) > 0 {
+		errorVal = data[0]
+	}
+
+	return &ErrorResponse{
+		Message:          err.Error(),
+		ErrorValidations: errorVal,
+	}
+}
+
+func removeFirstStruct(fields map[string]string) map[string]string {
+	res := map[string]string{}
+	for field, err := range fields {
+		res[field[strings.Index(field, ".")+1:]] = err
+	}
+	return res
+}
---
+++ utils/response/response.go
@@ -0,0 +1,71 @@
+package common
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Response struct {
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+type ResponseWithMeta struct {
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+	Meta    Meta   `json:"meta"`
+}
+
+type Meta struct {
+	Limit       int   `json:"limit"`
+	Total       int64 `json:"total"`
+	CurrentPage int   `json:"current_page"`
+	PrevPage    int   `json:"prev_page"`
+	NextPage    int   `json:"next_page"`
+	TotalPage   int   `json:"total_page"`
+}
+
+func NewResponse(ctx echo.Context, statusCode int, message string, data interface{}) error {
+	res := Response{
+		Message: message,
+		Data:    data,
+	}
+
+	return ctx.JSON(statusCode, res)
+}
+
+func NewResponseWithMeta(ctx echo.Context, statusCode int, message string, data interface{}, meta map[string]interface{}) error {
+	res := ResponseWithMeta{
+		Message: message,
+		Data:    data,
+		Meta:    setMeta(meta),
+	}
+
+	return ctx.JSON(statusCode, res)
+}
+
+func setMeta(meta map[string]interface{}) Meta {
+	var currentPage, totalPage, prevPage, nextPage int
+	currentPage = meta["currentPage"].(int)
+	totalPage = meta["totalPage"].(int)
+
+	if currentPage == totalPage {
+		nextPage = currentPage
+	} else {
+		nextPage = currentPage + 1
+	}
+
+	if currentPage == 1 {
+		prevPage = currentPage
+	} else {
+		prevPage = currentPage - 1
+	}
+
+	return Meta{
+		Limit:       meta["limit"].(int),
+		Total:       meta["total"].(int64),
+		CurrentPage: meta["currentPage"].(int),
+		PrevPage:    prevPage,
+		NextPage:    nextPage,
+		TotalPage:   totalPage,
+	}
+}
